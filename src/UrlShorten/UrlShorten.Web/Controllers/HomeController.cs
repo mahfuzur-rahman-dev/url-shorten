86d61efb79a20f11bbc1569fbd433daead088a74
Mahfuzur Rahman <mahfuzzz.cse@gmail.com>
2024-08-09 04:42:50 +0600

    sweetaleart2 working fine with create short url

--- src/UrlShorten/UrlShorten.Web/Controllers/HomeController.cs
+++ src/UrlShorten/UrlShorten.Web/Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 using UrlShorten.DataAccess.UnitOfWork;
+using UrlShorten.Models;
 using UrlShorten.Web.Models;
 
 namespace UrlShorten.Web.Controllers
@@ -24,12 +25,48 @@ namespace UrlShorten.Web.Controllers
         [HttpPost]
         public async Task<IActionResult> CheckShortKeywordAvailability(CheckAvailabilityRequestModel request)
         {
-            var checkShortKeyword = await _unitOfWork.Url.GetCountAsync(x=>x.ShortKeyword == request.ShortKeyword);
-            if(checkShortKeyword > 0)
+            var checkShortKeyword = await _unitOfWork.Url.GetCountAsync(x => x.ShortKeyword == request.ShortKeyword);
+            if (checkShortKeyword > 0)
                 return Json(new { Available = false });
             else
                 return Json(new { Available = true });
         }
+
+
+        [HttpPost]
+        public async Task<IActionResult> CreateShortUrl(CreateShortUrlRequestModel request)
+        {
+            if (request is null)
+            {
+                return BadRequest();
+            }
+
+            var addUrl = new Url
+            {
+                LongUrl = request.LongUrl,
+                Domain = request.Domain,
+                ShortKeyword = request.ShortKeyword,
+                ShortUrl = request.Domain + "/" + request.ShortKeyword,
+                CreatedDateTime = DateTime.Now,
+                UpdatedDateTime = DateTime.Now
+            };
+
+            try
+            {
+                await _unitOfWork.Url.AddAsync(addUrl);
+                await _unitOfWork.SaveAsync();
+
+                // Prepare data for the success response
+
+                return Json(new { CreateUrlStatus = true }); // Return JSON with success status and short URL
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return Json(new { CreateUrlStatus = false }); // Return JSON with failure status
+            }
+        }
+
 
         public IActionResult Privacy()
         {
